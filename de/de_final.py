bad59cf2ebfeb111703dcd4c02c7feb13e2486be
lhabich <lhabich@techfak.uni-bielefeld.de>
2023-10-22 19:35:23 +0000

    updated de and go

--- de/de_final.py
+++ de/de_final.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 from pydeseq2.dds import DeseqDataSet
 from pydeseq2.ds import DeseqStats
 import pandas as pd
@@ -33,7 +36,7 @@ def performDe(counts, metadata):
     dds.deseq2()
     return dds
 
-def createResultTable(dds, contrast1, contrast2):
+def createResultTable(dds, contrast1, contrast2, path):
     stat_res = DeseqStats(dds, n_cpus=8, contrast=('Condition', contrast1, contrast2))
     stat_res.summary()
     res = stat_res.results_df
@@ -43,39 +46,50 @@ def createResultTable(dds, contrast1, contrast2):
     cols_res = res.columns.tolist()
     cols_res = cols_res[-1:] + cols_res[:-1]
     ordered_res = res[cols_res]
-    ordered_res.to_csv('tables/result_table.tsv', sep="\t", index=False)
+    ordered_res.to_csv(path + '/tables/result_table.tsv', sep="\t", index=False)
     return res
 
-def identifySignificant(res):
+def identifySignificant(res, path):
     cols_res = res.columns.tolist()
     cols_res = cols_res[-1:] + cols_res[:-1]
     sigs = res[(res.padj < 0.05) & (abs(res.log2FoldChange) > 0.5)]
     ordered_sigs = sigs[cols_res]
-    ordered_sigs.to_csv('tables/significant_de.tsv', sep='\t', index=False)
+    ordered_sigs.to_csv(path + '/tables/significant_de.tsv', sep='\t', index=False)
     return sigs
-def performPCA(dds):
+def performPCA(dds, path):
     sc.pp.normalize_total(dds)
     sc.tl.pca(dds)
     sc.pl.pca(dds, color='Condition', size=250, color_map="magma", save="_normalized.svg")
+    shutil.move("figures/pca_normalized.svg", path + "/figures/pca_normalized.svg")
+    os.rmdir("/home/ubuntu/splivardet/gui/figures")
 
-def heatmapDE(dds, sigs):
+def heatmapDE(dds, sigs, path):
     dds.layers['log1p'] = np.log1p(dds.layers['normed_counts'])
     dds_sigs = dds[:, sigs.index]
     print(dds_sigs.obs_names)
     grapher = pd.DataFrame(dds_sigs.layers['log1p'].T,
                            index=dds_sigs.var_names, columns=dds_sigs.obs_names)
     sns.clustermap(grapher, z_score=0, cmap='Blues')
-    plt.savefig("figures/heatmap_de.svg", format='svg')
+    plt.savefig(path + "/figures/heatmap_de.svg", format='svg')
 
-def volcanoPlot(res):
-    volcano(res, symbol='Gene', colors=['blue', 'lightgrey', 'purple'], save="figures/volcano", to_label=5, top_right_frame=True)
+def volcanoPlot(res, path):
+    volcano(res, symbol='Gene', colors=['blue', 'lightgrey', 'purple'], save=path + "/figures/volcano", to_label=5, top_right_frame=True)
 
-
-counts = readCounts("file.tsv")
-metadata = createMetadata(counts, ['V', 'V', '0', 'V', '0', '0'])
-dds = performDe(counts, metadata)
-res = createResultTable(dds, '0', 'V')
-sigs = identifySignificant(res)
-performPCA(dds)
-heatmapDE(dds, sigs)
-volcanoPlot(res)
+def createDir(path):
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+        print("The new directory is created!")
+def main(data, metadata, contrast1, contrast2, path):
+    createDir(path + "/figures")
+    createDir(path + "/tables")
+    print("Start DE")
+    counts = readCounts(data)
+    metadata = createMetadata(counts, metadata)
+    dds = performDe(counts, metadata)
+    res = createResultTable(dds, contrast1,contrast2, path)
+    sigs = identifySignificant(res, path)
+    performPCA(dds, path)
+    heatmapDE(dds, sigs, path)
+    volcanoPlot(res, path)
+    print("Finished DE")
