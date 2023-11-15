1aa6b4d9525c18844a1063f39d4f45505c1eeca9
lhabich <lhabich@techfak.uni-bielefeld.de>
2023-11-15 14:58:32 +0000

    updated the implementation

--- gui/report_de.py
+++ gui/report_de.py
@@ -261,7 +261,7 @@ class Ui_ReportDe(object):
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:\'Segoe UI\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
-        self.label_9.setText(_translate("MainWindow", "Referenzgenom"))
+        self.label_9.setText(_translate("MainWindow", "Reference genome"))
         self.textEdit_4.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
